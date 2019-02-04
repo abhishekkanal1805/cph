e738aafe05ed77679c2fd8cbfa5fb73b38034960
Anuraj Pareek <anupareek@deloitte.com>
2019-02-04 09:02:27 +0000

    Merge pull request #34 in CHCONHUB/cph-base-services from bugfix/CHCONHUB-784 to development
    Squashed commit of the following:
    
    commit 70f1894094da2d1425776db280463688389812d2
    Author: US\vivekkmishra <vivekkmishra@deloitte.com>
    Date:   Mon Feb 4 14:25:52 2019 +0530
    
        Reverted changes.
    
    commit 7e8ed4cd897454443e6dec958cd60ac2c1659d72
    Merge: fa2beac cc2d1e1
    Author: US\vivekkmishra <vivekkmishra@deloitte.com>
    Date:   Mon Feb 4 13:41:58 2019 +0530
    
        Merge branch 'development' into bugfix/CHCONHUB-784
    
    commit fa2beacc0d9c671529a5ad4d4c6cb5c92f7946f2
    Author: US\vivekkmishra <vivekkmishra@deloitte.com>
    Date:   Mon Feb 4 01:37:15 2019 +0530
    
        Reverted changes.
    
    commit e9e637eb23a90afe0d75f403b5c183eb603869b8
    Merge: f8e3537 4473ae7
    Author: US\vivekkmishra <vivekkmishra@deloitte.com>
    Date:   Mon Feb 4 01:36:06 2019 +0530
    
        Merge branch 'development' into bugfix/CHCONHUB-784
    
    commit f8e35370790b81be9f53b0c75772f6bf8b5cf819
    Author: US\vivekkmishra <vivekkmishra@deloitte.com>
    Date:   Fri Feb 1 18:16:07 2019 +0530
    
        CHCONHUB-788 : Removed the constraint violation error stack check.
    
    commit 79e1dc7aab0da48f464b3abfce88cef46efb91c9
    Merge: 056abae 8a4615a
    Author: US\vivekkmishra <vivekkmishra@deloitte.com>
    Date:   Fri Feb 1 18:10:54 2019 +0530
    
        Merge branch 'development' into bugfix/CHCONHUB-784
    
    commit 056abaeda52d3301fbbeec627888586014aa5d8e
    Merge: 90237df 0ac579c
    Author: US\vivekkmishra <vivekkmishra@deloitte.com>
    Date:   Fri Feb 1 12:29:44 2019 +0530
    
        Merge branch 'development' into bugfix/CHCONHUB-784
    
    commit 90237df398ffd4e7aeb2b247dcadacca6a8e1e31
    Author: US\vivekkmishra <vivekkmishra@deloitte.com>
    Date:   Tue Jan 29 16:47:41 2019 +0530
    
        CHCONHUB-784 CHCONHUB-788 CHCONHUB-791 CHCONHUB-793 CHCONHUB-796 : Bug fixes

--- services/connection/connectionService.ts
+++ services/connection/connectionService.ts
@@ -1,7 +1,7 @@
 import * as log from "lambda-log";
 import { errorCode } from "../../common/constants/error-codes";
 import * as config from "../../common/objects/config";
-import { BadRequestResult } from "../../common/objects/custom-errors";
+import { BadRequestResult, UnAuthorizedResult } from "../../common/objects/custom-errors";
 import { UserProfile } from "../../models/CPH/userProfile/userProfile";
 import { DataService } from "../common/dataService";
 
@@ -22,7 +22,7 @@ export class ConnectionService {
     let mandatoryAttribute;
     if (["practitioner", "careteam", "patient"].indexOf(userProfile.type.toLowerCase()) === -1) {
       log.error("Error occoured due to invalid userType: " + userProfile.type);
-      throw new BadRequestResult(errorCode.InvalidRequest, "User is not authorized to perform this operation");
+      throw new UnAuthorizedResult(errorCode.UnauthorizedUser, "User Validation Failed");
     }
     // Condition added for Patient. Patient must give his id in from
     // using this attibute to validate whether its a userProfile or not
@@ -30,7 +30,7 @@ export class ConnectionService {
       // Validate from and loggedinId both are same for patient
       if (queryParams.hasOwnProperty("from") && queryParams["from"] != loggedinId) {
         log.error("Error occoured due to patient type");
-        throw new BadRequestResult(errorCode.InvalidRequest, "User is not authorized to perform this operation");
+        throw new UnAuthorizedResult(errorCode.UnauthorizedUser, "User Validation Failed");
       } else if (queryParams.hasOwnProperty("to")) {
         if (queryParams["to"] != loggedinId) {
           queryParams["from"] = loggedinId;
@@ -48,7 +48,7 @@ export class ConnectionService {
       // Validate to and loggedinId both are same for partner and delegate
       if (queryParams.hasOwnProperty("to") && queryParams["to"] != loggedinId) {
         log.error("Error occoured due to practitioner/careteam type");
-        throw new BadRequestResult(errorCode.InvalidRequest, "User is not authorized to perform this operation");
+        throw new UnAuthorizedResult(errorCode.UnauthorizedUser, "User Validation Failed");
       } else if (queryParams.hasOwnProperty("from")) {
         if (queryParams["from"] != loggedinId) {
           queryParams["to"] = loggedinId;
@@ -64,7 +64,7 @@ export class ConnectionService {
       }
     } else {
       log.error("Error occoured due to invalid type");
-      throw new BadRequestResult(errorCode.InvalidRequest, "User is not authorized to perform this operation");
+      throw new UnAuthorizedResult(errorCode.UnauthorizedUser, "User Validation Failed");
     }
     log.info("queryParams", queryParams);
     if (queryParams.hasOwnProperty("to")) {
@@ -90,6 +90,9 @@ export class ConnectionService {
       performUserValidation,
       appendUserProfile
     );
+    for (const record of searchResult) {
+      this.getUpdatedResponse(record);
+    }
     return searchResult;
   }
 
@@ -129,5 +132,18 @@ export class ConnectionService {
       profileDisplay[referenceId] = userProfileObj.name ? givenName.join(" ") : "";
     }
     return userProfileObj;
+  }
+
+  /**
+   * To update display attributes in response object
+   * @param {any} responseObj response object
+   * @returns {Promise<any>}
+   */
+  public static getUpdatedResponse(responseObj: any): Promise<any> {
+    const referenceValFrom = responseObj.from.reference.split("/").reverse()[0];
+    const referenceValTo = responseObj.to.reference.split("/").reverse()[0];
+    responseObj.from.display = profileDisplay.hasOwnProperty(referenceValFrom) ? profileDisplay[referenceValFrom] : "";
+    responseObj.to.display = profileDisplay.hasOwnProperty(referenceValTo) ? profileDisplay[referenceValTo] : "";
+    return responseObj;
   }
 }
