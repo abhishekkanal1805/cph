30f938f60b8df9faeabd4af1d97b3b11e358c158
US\rohmane <rohmane@deloitte.com>
2019-04-18 16:17:15 +0530

    CHCONHUB-1315 Created seperate function to update FHIR resources

--- services/utilities/dataFetchSpec.ts
+++ services/utilities/dataFetchSpec.ts
@@ -60,7 +60,7 @@ describe("Test getUserProfile() - ", () => {
     let result;
     const expected = new ForbiddenResult(errorCodeMap.Forbidden.value, errorCodeMap.Forbidden.description);
     try {
-       await DataFetch.getUserProfile(["80e86ff5-3527-44b0-b2d2-743bfe770b29", "137cbafa-3ac0-48a9-a390-e0d5982a0d05"]);
+      await DataFetch.getUserProfile(["80e86ff5-3527-44b0-b2d2-743bfe770b29", "137cbafa-3ac0-48a9-a390-e0d5982a0d05"]);
     } catch (err) {
       result = err;
     }
