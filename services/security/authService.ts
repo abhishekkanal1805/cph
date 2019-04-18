1d5ff54ffb86eb0f4a6fdf3c51ee0b760887f5b4
Debabrata Pati <depati@deloitte.com>
2019-04-18 19:52:35 +0530

    DEV: fixed for get and search

--- services/security/authService.ts
+++ services/security/authService.ts
@@ -73,7 +73,7 @@ export class AuthService {
    * @memberof AuthService
    */
   public static async authorizeConnectionBased(requesterId: string, requesteeId: string) {
-    log.info("Inside AuthService :: hasConnectionBasedAccess()");
+    log.info("Inside AuthService :: authorizeConnectionBased()");
     const requestProfileIds = [requesterId, requesteeId];
     // query userprofile for the unique profile ids
     const fetchedProfiles = await DataFetch.getUserProfile(requestProfileIds);
@@ -107,8 +107,8 @@ export class AuthService {
    */
   public static async hasConnection(from: string, to: string, type: string[], status: string[]) {
     log.info("Inside AuthService :: hasConnection()");
-    from = (from.indexOf(Constants.USERPROFILE_REFERENCE) == -1) ? (Constants.USERPROFILE_REFERENCE + from) : from;
-    to = (to.indexOf(Constants.USERPROFILE_REFERENCE) == -1) ? (Constants.USERPROFILE_REFERENCE + to) : to;
+    from = from.indexOf(Constants.USERPROFILE_REFERENCE) == -1 ? Constants.USERPROFILE_REFERENCE + from : from;
+    to = to.indexOf(Constants.USERPROFILE_REFERENCE) == -1 ? Constants.USERPROFILE_REFERENCE + to : to;
     const queryOptions = {
       where: {
         "from.reference": from,
