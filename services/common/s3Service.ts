bec0b0200b07828ad8a489b9de0297f8bffb95d4
Anuraj Pareek <anupareek@deloitte.com>
2019-04-24 13:33:44 +0530

    bump typescript version and added s3 functions

--- services/common/s3Service.ts
+++ services/common/s3Service.ts
@@ -1,7 +1,7 @@
 import * as AWS from "aws-sdk";
 import * as log from "lambda-log";
 import { errorCodeMap } from "../../common/constants/error-codes-map";
-import { InternalServerErrorResult } from "../../common/objects/custom-errors";
+import { InternalServerErrorResult, NotFoundResult } from "../../common/objects/custom-errors";
 const s3 = new AWS.S3();
 
 export class S3Service {
@@ -30,6 +30,56 @@ export class S3Service {
       .catch((err) => {
         log.error("Error in uploading object to S3 bucket", err);
         throw new InternalServerErrorResult(errorCodeMap.InternalError.value, errorCodeMap.InternalError.description);
+      });
+  }
+
+  /**
+   * returns the requested object for the specified key from s3 bucket.
+   * @param {string} bucket
+   * @param {string} key
+   * @returns {Promise<S3.Body>}
+   */
+  public static async get(bucket: string, key: string) {
+    log.info("Inside S3Service: get()");
+    const paramsToGetObject = {
+      Bucket: bucket,
+      Key: key
+    };
+    return await s3
+      .getObject(paramsToGetObject)
+      .promise()
+      .then((data) => {
+        log.info("Object fetched from  S3 successfully");
+        return data;
+      })
+      .catch((err) => {
+        log.error("Error in fetching object from S3 bucket", err);
+        throw new NotFoundResult(errorCodeMap.NotFound.value, errorCodeMap.NotFound.description);
+      });
+  }
+
+  /**
+   * Deletes  object for the specified key from s3 bucket.
+   * @param {string} bucket
+   * @param {string} key
+   * @returns {Promise<S3.Body>}
+   */
+  public static async delete(bucket: string, key: string) {
+    log.info("Inside S3Service: delete()");
+    const paramsToDeleteObject = {
+      Bucket: bucket,
+      Key: key
+    };
+    return await s3
+      .deleteObject(paramsToDeleteObject)
+      .promise()
+      .then((data) => {
+        log.info("Object deleted from  S3 successfully");
+        return data;
+      })
+      .catch((err) => {
+        log.error("Error in deleting object from S3 bucket", err);
+        throw new NotFoundResult(errorCodeMap.NotFound.value, errorCodeMap.NotFound.description);
       });
   }
 
