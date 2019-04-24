2af16074fd1d77bb2d0657b7fda002c2b6c4b7d3
Ankur Shah <ankurshah2@deloitte.com>
2019-04-24 10:54:12 -0400

    CHCONHUB-992 missed the constants file in prev commit

--- common/constants/constants.ts
+++ common/constants/constants.ts
@@ -138,6 +138,7 @@ class Constants {
   public static readonly PERIOD_MONTHS = "months";
   public static readonly PERIOD_YEARS = "years";
 
+  public static readonly RESOURCE_ACCESSIBLE_TO_ALL = ["questionnaire", "device"];
   public static readonly DEFAULT_SEARCH_ATTRIBUTES = "dataResource";
   public static readonly DEFAULT_ORDER_BY = [["meta.lastUpdated", "DESC"]];
 
