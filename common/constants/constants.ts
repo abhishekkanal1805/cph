2e6b206a4c4ac99f5e0e7daea4fa3740798392c0
viav <viav@deloitte.com>
2019-04-24 11:10:37 +0530

    CHCONHUB-1195

--- common/constants/constants.ts
+++ common/constants/constants.ts
@@ -134,6 +134,12 @@ class Constants {
 
   public static readonly DEFAULT_SEARCH_ATTRIBUTES = "dataResource";
   public static readonly DEFAULT_ORDER_BY = [["meta.lastUpdated", "DESC"]];
+
+  public static readonly QUESTIONNAIRE_TITLE_IMAGE = "titleImage";
+  public static readonly TITLE_IMAGE_CONTENT_TYPE = "contentType";
+  public static readonly TITLE_IMAGE_CREATION = "creation";
+  public static readonly TITLE_IMAGE_URL = "url";
+
 }
 
 export { Constants };
