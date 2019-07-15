e6a4ab6ff10b8267916ff100cd674751a63a80c9
Ankur Shah <ankurshah2@deloitte.com>
2019-07-15 12:04:09 -0400

    CHCONHUB-1426 coding.display is allowed to be translated

--- models/common/coding.ts
+++ models/common/coding.ts
@@ -1,8 +1,13 @@
+import { Extension } from "./extension";
+
 class Coding {
   version: string;
   userSelected: boolean;
   system: string;
   code: string;
   display: string;
+
+  /* Extensions */
+  _display: Extension;
 }
 export { Coding };
