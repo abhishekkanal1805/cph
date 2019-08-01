0f1cca20753f7115aede6be89a551387d256b75f
Debabrata Pati <depati@deloitte.com>
2019-08-01 17:42:11 +0530

    TST: test cases added for CHCONHUB-3898 CHCONHUB-3899

--- services/utilities/sharingRulesHelperSpec.ts
+++ services/utilities/sharingRulesHelperSpec.ts
@@ -1,87 +1,85 @@
 import "jasmine";
-import * as moment from "moment";
-import { Op } from "sequelize";
-import { Constants } from "../../common/constants/constants";
+// import * as log from "lambda-log";
+// import * as moment from "moment";
+// import { Op } from "sequelize";
+// import { Constants } from "../../common/constants/constants";
 import { SharingRulesHelper } from "./sharingRulesHelper";
 
 describe("SharingRulesHelper", () => {
   describe("#expressionEvaluator()", () => {
-    it("Returns date if only month is present in expression", (done) => {
-      const expression = "(JANUARY)";
-      const expected = "2019-01-31";
-      const result = SharingRulesHelper.expressionEvaluator(expression);
-      expect(result).toEqual(expected);
-      done();
-    });
-    it("Returns date if only day is present in expression", (done) => {
-      const expression = "(MONDAY)";
-      const days: any = Constants.DAYS_IN_WEEK;
-      const parenthesesStart: number = expression.indexOf(Constants.OPENING_PARENTHESES);
-      const parenthesesEnd: number = expression.indexOf(Constants.CLOSING_PARENTHESES);
-      const value: string = expression.substr(parenthesesStart + 1, parenthesesEnd - parenthesesStart - 1);
-      const daydiff = moment().day() <= days[value] ? days[value] - 7 : days[value];
-      const expected = moment()
-        .day(daydiff)
-        .format("YYYY-MM-DD");
-      const result = SharingRulesHelper.expressionEvaluator(expression);
-      expect(result).toEqual(expected);
-      done();
-    });
-    it("Returns date if only year is present in expression", (done) => {
-      const expression = "(2019)";
+    it("Returns last date for current year in  format if dateOfLast(year) is present in expression", (done) => {
+      const expression = "dateOfLast(2019)";
       const expected = "2019-12-31";
       const result = SharingRulesHelper.expressionEvaluator(expression);
       expect(result).toEqual(expected);
       done();
     });
-  });
-
-  describe("#getCriteriaClause()", () => {
-    it("Returns conditions based on criteria", (done) => {
-      const criteria = [
-        {
-          type: "single",
-          element: "dateAsserted",
-          operation: "greaterThanOrEqual",
-          value: "2022-02-02"
-        },
-        {
-          type: "single",
-          element: "category[*].coding[*].code",
-          operation: "vital-sign",
-          value: "vital-sign"
-        }
-      ];
-      spyOn(SharingRulesHelper, "generateConditionForSingleCriteria").and.callFake((criterion, operationMap) => {
-        return {
-          ["dateAsserted"]: {
-            [Op.gte]: "2022-02-02"
-          }
-        };
-      });
-      const expected: any = {
-        [Op.or]: [
-          {
-            ["dateAsserted"]: {
-              [Op.gte]: "2022-02-02"
-            },
-            ["category"]: {
-              [Op.contains]: [
-                {
-                  coding: {
-                    code: "vital-sign"
-                  }
-                }
-              ]
-            }
-          }
-        ]
-      };
-      const operator = Op.or;
-      const result = {};
-      result[operator] = [];
-      result[operator].push(SharingRulesHelper.generateConditionForSingleCriteria(criteria[0], { greaterThan: [Op.gt, "gt"] }));
-      result[operator].push(SharingRulesHelper.generateConditionForSingleCriteria(criteria[1], { equal: [Op.eq, ""] }));
+    it("Returns last date for current year in  format if dateOfLast(year) is present in expression", (done) => {
+      const expression = "dateOfLast(19)";
+      const expected = "19-12-31";
+      const result = SharingRulesHelper.expressionEvaluator(expression);
+      expect(result).toEqual(expected);
+      done();
+    });
+    it("Returns last date for current year in YYYY-MM-DD format if dateOfLast(month_fullname) is present in expression", (done) => {
+      const expression = "dateOfLast(FEBRUARY)";
+      jasmine.clock().mockDate(new Date("2019-03-01"));
+      const expected = "2019-02-28";
+      const result = SharingRulesHelper.expressionEvaluator(expression);
+      expect(result).toEqual(expected);
+      done();
+    });
+    it("Returns last date for current year in YYYY-MM-DD format if dateOfLast(month_fullname) is present in expression", (done) => {
+      const expression = "dateOfLast(FEBRUARY)";
+      jasmine.clock().mockDate(new Date("2020-03-01"));
+      const expected = "2020-02-29";
+      const result = SharingRulesHelper.expressionEvaluator(expression);
+      expect(result).toEqual(expected);
+      done();
+    });
+    it("Returns last date for current year in YYYY-MM-DD format if dateOfLast(month_shortname) is present in expression", (done) => {
+      const expression = "dateOfLast(FEB)";
+      jasmine.clock().mockDate(new Date("2020-02-01"));
+      const expected = "2020-02-29";
+      const result = SharingRulesHelper.expressionEvaluator(expression);
+      expect(result).toEqual(expected);
+      done();
+    });
+    it("Returns last date for current year in YYYY-MM-DD format if dateOfLast(month_shortname) is present in expression", (done) => {
+      const expression = "dateOfLast(JAN)";
+      jasmine.clock().mockDate(new Date("2019-02-01"));
+      const expected = "2019-01-31";
+      const result = SharingRulesHelper.expressionEvaluator(expression);
+      expect(result).toEqual(expected);
+      done();
+    });
+    it("Returns invalid_name-MM-DD if dateOfLast(invalid_name) is present in expression", (done) => {
+      const expression = "dateOfLast(JA)";
+      const expected = "JA-12-31";
+      const result = SharingRulesHelper.expressionEvaluator(expression);
+      expect(result).toEqual(expected);
+      done();
+    });
+    it("Returns -MM-DD format if month_fullname without brackets is present in expression", (done) => {
+      const expression = "JANUARY";
+      const expected = "-12-31";
+      const result = SharingRulesHelper.expressionEvaluator(expression);
+      expect(result).toEqual(expected);
+      done();
+    });
+    it("Returns previous week monday in YYYY-MM-DD format if current day is monday and dateOfLast(MONDAY) present in sharing rules expression", (done) => {
+      const expression = "dateOfLast(MONDAY)";
+      jasmine.clock().mockDate(new Date("2019-07-29"));
+      const expected = "2019-07-22";
+      const result = SharingRulesHelper.expressionEvaluator(expression);
+      expect(result).toEqual(expected);
+      done();
+    });
+    it("Returns current week monday in YYYY-MM-DD format if current day is Tuesday and dateOfLast(MONDAY) present in sharing rules expression", (done) => {
+      const expression = "dateOfLast(MONDAY)";
+      jasmine.clock().mockDate(new Date("2019-07-30"));
+      const expected = "2019-07-29";
+      const result = SharingRulesHelper.expressionEvaluator(expression);
       expect(result).toEqual(expected);
       done();
     });
