e08175b313dbf91cf0c10094daa74dab5adf0f8c
Ankur Shah <ankurshah2@deloitte.com>
2020-02-09 18:01:35 -0500

    CHCONHUB-5160 using the reference utility

--- services/dao/researchSubjectDAO.ts
+++ services/dao/researchSubjectDAO.ts
@@ -8,33 +8,27 @@ import { IFindOptions } from "sequelize-typescript";
 import { Constants } from "../../common/constants/constants";
 import { ResearchSubject } from "../../models/CPH/researchSubject/researchSubject";
 import { ResearchSubjectDataResource } from "../../models/CPH/researchSubject/researchSubjectDataResource";
+import {ReferenceUtility} from "../utilities/referenceUtility";
 import { DAOService } from "./daoService";
 
 class ResearchSubjectDAO {
   /**
-   * Gets the ResearchSubject by references from database
+   * It selects unique ResearchSubject references from the provided list and retrieves non deleted resources from database
    * @param references
    */
   public static async getByReferences(references: string[]): Promise<ResearchSubjectDataResource[]> {
     // filtering out duplicates and empty values
     log.info("ResearchSubjectDAO - References requested: " + JSON.stringify(references));
-    const uniqueAndValidReferences = [...new Set(references)].filter(Boolean);
-    const researchSubjectIds: string[] = uniqueAndValidReferences.map((researchSubjectReference) => {
-      if (!researchSubjectReference.startsWith(Constants.RESEARCHSUBJECT_REFERENCE)) {
-        log.info("ResearchSubjectDAO - reference provided is not for a ResearchSubject. entry=" + researchSubjectReference);
-        return null;
-      }
-      return researchSubjectReference.split(Constants.RESEARCHSUBJECT_REFERENCE)[1];
-    });
-
-    if (researchSubjectIds.length < 1) {
+    const uniqueSubjectReferences = ReferenceUtility.getUniqueReferences(references, Constants.RESEARCHSUBJECT_REFERENCE);
+    if (uniqueSubjectReferences.length < 1) {
       log.info("ResearchSubjectDAO - no valid ResearchSubject references found, returning null");
       return null;
     }
+    log.info("ResearchSubjectDAO - unique and subject References: " + JSON.stringify(uniqueSubjectReferences));
 
     const researchSubjectQuery: IFindOptions<ResearchSubject> = {
       where: {
-        id: researchSubjectIds,
+        id: ReferenceUtility.convertToResourceIds(uniqueSubjectReferences, Constants.RESEARCHSUBJECT_REFERENCE),
         meta: {
           isDeleted: false
         }
